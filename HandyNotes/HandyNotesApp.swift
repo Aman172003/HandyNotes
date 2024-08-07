aa98ca0ae8df17ccee5d65b0f8e61c465fd57a12
Aman172003 <tej171995@gmail.com>
2024-08-08 02:48:24 +0530

    Initial Commit

---
+++ HandyNotes/HandyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandyNotesApp.swift
+//  HandyNotes
+//
+//  Created by Aman on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HandyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
